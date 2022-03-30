7090b248a3640ce60f514d5347bf3b71bd7b3230
KAKHA13 <kakhagiorgashvili@gmail.com>
2022-03-30 22:27:20 +0400

    init

--- app/app.js
+++ app/app.js
@@ -2,6 +2,9 @@ import Vue from 'nativescript-vue'
 
 import App from './components/App'
 
+import BottomSheetPlugin from '@nativescript-community/ui-persistent-bottomsheet/vue';
+Vue.use(BottomSheetPlugin);
+
 new Vue({
   render: (h) => h(App),
 }).$start()
--- app/components/App.vue
+++ app/components/App.vue
@@ -1,44 +1,16 @@
-<template lang="html">
-    <TabView androidTabsPosition="bottom">
-        <TabViewItem title="Home">
-            <Frame id="items">
-                <Items/>
-            </Frame>
-        </TabViewItem>
-
-        <TabViewItem title="Browse">
-            <Frame id="browse">
-                <Browse/>
-            </Frame>
-        </TabViewItem>
-
-        <TabViewItem title="Search">
-            <Frame id="search">
-                <Search/>
-            </Frame>
-        </TabViewItem>
-    </TabView>
+<template>
+    <Page>
+    <BottomSheet>
+        <StackLayout backgroundColor="white">
+            <Label text="This is the main content" />
+        </StackLayout>
+        <GridLayout ~bottomSheet backgroundColor="white" height="70">
+            <Label text="This is the side drawer content" />
+        </GridLayout>
+    </BottomSheet>
+    </Page>
 </template>
 
 <script>
-  import Items from "./Items.vue";
-  import Browse from "./Browse.vue";
-  import Search from "./Search.vue";
 
-  export default {
-    components: {
-      Items,
-      Browse,
-      Search
-    }
-  };
 </script>
-
-<style lang="scss">
-    // Start custom common variables
-    @import "@nativescript/theme/scss/variables/blue";
-    // End custom common variables
-
-    // Custom styles
-
-</style>
